971233f96d08a1d39026978e12e3f1107557dcbd
Michael B Ansong <michael.ansong7@gmail.com>
2020-12-27 19:51:53 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,15 @@
-# Write your code here
+
+#count=10
+#while count <=1 do
+ # count_= count - 1
+  #puts count_
+#end
+#puts "Happy New Year!"
+
 count=10
 while count >=1 do
-  count_=1
+  count-=1
   puts count
 end
 puts "Happy New Year!"
+
